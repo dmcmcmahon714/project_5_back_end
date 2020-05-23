e29ee729792d43f8c68f5ca42e51f07e2dc3a13f
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-05-22 22:06:10 -0500

    started sessions

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login with invalid information" do
+    get new_user_session_path
+    assert_template 'sessions/new'
+    post new_user_session_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
